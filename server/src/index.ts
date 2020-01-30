d13dd144fe43aab70ad68fb342fc0cfa91486ddd
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2020-01-31 02:11:28 +0400

    commit 14

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import express from 'express';
 const app = express();
 const port = 9000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (_req, res) => res.send("Hello World!"));
 
 app.listen(port);
 
