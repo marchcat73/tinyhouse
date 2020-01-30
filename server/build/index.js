c8c29bd157341d4cdd66371f7722348907e32c11
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2020-01-31 00:00:42 +0400

    commit 13

---
+++ server/build/index.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// const express = require("express");
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+const port = 9000;
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port);
+console.log(`[app]: http://localhost:${port}`);
