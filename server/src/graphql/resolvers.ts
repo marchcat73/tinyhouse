bbbb91c11e6f4595bd2889a1c855f4e6df7c2262
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2020-02-09 21:53:38 +0400

    commit 4.7

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,22 +1,40 @@
+import { ObjectId } from 'mongodb';
 import { IResolvers } from 'apollo-server-express';
+import { Database, Listing } from '../lib/types';
 import { listings } from '../listings';
 
 export const resolvers: IResolvers = {
   Query: {
-    listings: () => {
-      return listings;
+    listings: async (
+      _root: undefined,
+      _args: {},
+      { db }: { db: Database }
+    ): Promise<Listing[]> => {
+      return await db.listings.find({}).toArray();
     }
   },
 
   Mutation: {
-    deleteListing: (_root: undefined, { id }: { id: string }) => {
-      for (let i = 0; i < listings.length; i++) {
-        if (listings[i].id === id) {
-          return listings.splice(i, 1)[0];
-        }
+    deleteListing: async (
+      _root: undefined,
+      { id }: { id: string },
+      { db }: { db: Database }
+    ): Promise<Listing> => {
+      const deleteRes = await db.listings.findOneAndDelete({
+        _id: new ObjectId(id)
+      });
+
+      if (!deleteRes.value) {
+        throw new Error('failed to delete listing');
       }
 
-      throw new Error('failed to delete listing');
+      return deleteRes.value;
     }
+  },
+
+  Listing: {
+    // title: (listing: Listing) => listing.title,
+    // image: (listing: Listing) => listing.image
+    id: (listing: Listing): string => listing._id.toString()
   }
 };
