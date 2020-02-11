dde0bf36aa4b72917724141550fd75c2b5c1623e
Vitaliy Vanyushin <vitalij.vanyushin@simbirsoft.com>
2020-02-11 13:12:47 +0400

    commit 7.6

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -35,7 +35,7 @@ interface Props {
 }
 
 export const Listings = ({ title }: Props) => {
-  const { data, refetch } = useQuery<ListingsData>(LISTINGS);
+  const { data, loading, error, refetch } = useQuery<ListingsData>(LISTINGS);
 
   const deleteListing = async (id: string) => {
     await server.fetch<DeleteListingData, DeleteListingVariables>({
@@ -62,6 +62,14 @@ export const Listings = ({ title }: Props) => {
       })}
     </ul>
   ) : null;
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>Error</h2>;
+  }
 
   return (
     <div>
