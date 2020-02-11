e85583d08feacbfb2492676e879483736892e270
Vitaliy Vanyushin <vitalij.vanyushin@simbirsoft.com>
2020-02-11 11:09:31 +0400

    commit 7.4

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { server } from '../../lib/api';
+import React from 'react';
+import { server, useQuery } from '../../lib/api';
 import {
   ListingsData,
   DeleteListingData,
-  DeleteListingVariables,
-  Listing
+  DeleteListingVariables
 } from './types';
 
 const LISTINGS = `
@@ -36,16 +35,7 @@ interface Props {
 }
 
 export const Listings = ({ title }: Props) => {
-  const [listings, setListings] = useState<Listing[] | null>(null);
-
-  useEffect(() => {
-    fetchListings();
-  }, []);
-
-  const fetchListings = async () => {
-    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
-    setListings(data.listings);
-  };
+  const { data } = useQuery<ListingsData>(LISTINGS);
 
   const deleteListing = async (id: string) => {
     await server.fetch<DeleteListingData, DeleteListingVariables>({
@@ -54,9 +44,9 @@ export const Listings = ({ title }: Props) => {
         id
       }
     });
-
-    fetchListings();
   };
+
+  const listings = data ? data.listings : null;
 
   const listingsList = listings ? (
     <ul>
