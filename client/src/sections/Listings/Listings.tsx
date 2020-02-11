973b32c969d7a1e1f628d5be0f0377a64009bc42
Vitaliy Vanyushin <vitalij.vanyushin@simbirsoft.com>
2020-02-11 16:05:10 +0400

    commit 8.2

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { useQuery, useMutation } from '../../lib/api';
+import { gql } from 'apollo-boost';
+import { useQuery, useMutation } from 'react-apollo';
 import {
   ListingsData,
   DeleteListingData,
   DeleteListingVariables
 } from './types';
 
-const LISTINGS = `
+const LISTINGS = gql`
   query Listings {
     listings {
-      id 
-      title 
+      id
+      title
       image
       address
       price
@@ -22,7 +23,7 @@ const LISTINGS = `
   }
 `;
 
-const DELETE_LISTING = `
+const DELETE_LISTING = gql`
   mutation DeleteListing($id: ID!) {
     deleteListing(id: $id) {
       id
@@ -43,7 +44,7 @@ export const Listings = ({ title }: Props) => {
   ] = useMutation<DeleteListingData, DeleteListingVariables>(DELETE_LISTING);
 
   const handleDeleteListing = async (id: string) => {
-    await deleteListing({ id });
+    await deleteListing({ variables: { id } });
 
     refetch();
   };
