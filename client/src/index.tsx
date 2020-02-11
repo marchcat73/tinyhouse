a44863fe7bfe1a9d5e315277edf5cccbd3eb5a4c
Vitaliy Vanyushin <vitalij.vanyushin@simbirsoft.com>
2020-02-11 15:50:00 +0400

    commit 8.1

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,19 @@
-import React from "react";
-import { render } from "react-dom";
-import { Listings } from "./sections";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import { render } from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { Listings } from './sections';
+import * as serviceWorker from './serviceWorker';
+
+const client = new ApolloClient({
+  uri: '/api'
+});
 
 render(
-  <Listings title="TinyHouse Listings" />,
-  document.getElementById("root")
+  <ApolloProvider client={client}>
+    <Listings title="TinyHouse Listings" />
+  </ApolloProvider>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
