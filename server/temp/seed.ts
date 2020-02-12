e7d8978f7dbd48c5e607e2db3e258dc4d053da04
Vitaliy Vanyushin <vitalij.vanyushin@simbirsoft.com>
2020-02-12 17:28:53 +0400

    commit 81

--- server/temp/seed.ts
+++ server/temp/seed.ts
@@ -4,7 +4,7 @@ import { ObjectId } from 'mongodb';
 import { connectDatabase } from '../src/database';
 import { Listing, ListingType, User } from '../src/lib/types';
 
-const listing: Listing[] = [];
+const listings: Listing[] = [];
 
 const users: User[] = [];
 
@@ -13,47 +13,13 @@ const seed = async () => {
     console.log('[seed] : running...');
 
     const db = await connectDatabase();
-    const listings: Listing[] = [
-      {
-        _id: new ObjectId(),
-        title: 'Clean and fully furnished apartment. 5 min away from CN Tower',
-        image:
-          'https://res.cloudinary.com/tiny-house/image/upload/v1560641352/mock/Toronto/toronto-listing-1_exv0tf.jpg',
-        address: '3210 Scotchmere Dr W, Toronto, ON, CA',
-        price: 10000,
-        numOfGuests: 2,
-        numOfBeds: 1,
-        numOfBaths: 2,
-        rating: 5
-      },
-      {
-        _id: new ObjectId(),
-        title: 'Luxurious home with private pool',
-        image:
-          'https://res.cloudinary.com/tiny-house/image/upload/v1560645376/mock/Los%20Angeles/los-angeles-listing-1_aikhx7.jpg',
-        address: '100 Hollywood Hills Dr, Los Angeles, California',
-        price: 15000,
-        numOfGuests: 2,
-        numOfBeds: 1,
-        numOfBaths: 1,
-        rating: 4
-      },
-      {
-        _id: new ObjectId(),
-        title: 'Single bedroom located in the heart of downtown San Fransisco',
-        image:
-          'https://res.cloudinary.com/tiny-house/image/upload/v1560646219/mock/San%20Fransisco/san-fransisco-listing-1_qzntl4.jpg',
-        address: '200 Sunnyside Rd, San Fransisco, California',
-        price: 25000,
-        numOfGuests: 3,
-        numOfBeds: 2,
-        numOfBaths: 2,
-        rating: 3
-      }
-    ];
 
     for (const listing of listings) {
       await db.listings.insertOne(listing);
+    }
+
+    for (const user of users) {
+      await db.users.insertOne(user);
     }
 
     console.log('[seed] : success');
